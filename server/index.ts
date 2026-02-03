863a9f3e66f49248f29f8c3b74179e05f57e6c8d
Samuel Aaron <samiroyal111@Gmail.com>
2026-02-03 20:34:48 +0000

    deploy

--- server/index.ts
+++ server/index.ts
@@ -90,13 +90,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-    {
-      port,
-      host: "0.0.0.0",
-      reusePort: true,
-    },
-    () => {
+ app.listen(5000, "localhost", () => {
       log(`serving on port ${port}`);
     },
   );
